23a8d719e1a66ecb231d412d310af3d8587a7494
Andreas Mårtensson <addem@addem.se>
2019-03-06 18:40:56 +0100

    Reorganize state.ex

--- lib/firestone/engine/state.ex
+++ lib/firestone/engine/state.ex
@@ -1,85 +1,4 @@
 defmodule Firestone.Engine.State do
-  def default_player(player_id) do
-    %{
-      hero: Firestone.Engine.Entities.create("Rexxar", :hero, player_id),
-      board: [],
-      graveyard: [],
-      hand: [],
-      deck: [],
-      fatigue: 0,
-      mana: 0,
-      mana_crystals: 0
-    }
-  end
-
-  def add_player_entity(state, player_id, type, data) do
-    entity = Firestone.Engine.Entities.create(data, type, state.counter)
-
-    state
-    |> put_in([:players, player_id, type], entity)
-    |> update_in([:counter], &(&1 + 1))
-  end
-
-  def add_player_entity_list(state, player_id, type, list) do
-    entities =
-      for {data, c} <- Enum.with_index(list, state.counter) do
-        Firestone.Engine.Entities.create(data, type, c)
-      end
-
-    state
-    |> update_in([:players, player_id, type], &(entities ++ &1))
-    |> update_in([:counter], &(&1 + length(entities)))
-  end
-
-  def add_player_data(state, player_id, data) do
-    initial_player = put_in(state.players[player_id], default_player(player_id))
-
-    for {key, val} <- data, reduce: initial_player do
-      state ->
-        cond do
-          key in [:hero] ->
-            add_player_entity(state, player_id, key, val)
-
-          key in [:board, :hand, :deck, :graveyard] ->
-            add_player_entity_list(state, player_id, key, val)
-
-          true ->
-            put_in(state.players[player_id][key], val)
-        end
-    end
-  end
-
-  def add_turn_entity_list(state, key, list) do
-    entities =
-      for {data, c} <- Enum.with_index(list, state.counter) do
-        Firestone.Engine.Entities.create(data, key, c)
-      end
-
-    state
-    |> update_in([:turn, key], &(&1 ++ entities))
-    |> update_in([:counter], &(&1 + length(entities)))
-  end
-
-  def add_turn_data(state, data) do
-    for {key, val} <- data, reduce: state do
-      state ->
-        cond do
-          key in [:cards_played, :minions_summoned] -> add_turn_entity_list(state, key, val)
-          true -> put_in(state.turn[key], val)
-        end
-    end
-  end
-
-  def add_data(state, data) do
-    for {key, val} <- data, reduce: state do
-      state ->
-        cond do
-          key in [:turn] -> add_turn_data(state, val)
-          true -> put_in(state[key], val)
-        end
-    end
-  end
-
   @doc """
 
   Top level keys work:
@@ -150,11 +69,92 @@ defmodule Firestone.Engine.State do
         cards_played: [],
         minions_summoned: []
       },
-      players: %{}
+      players: %{p1: default_player(:p1), p2: default_player(:p2)}
     }
     |> add_player_data(:p1, p1)
     |> add_player_data(:p2, p2)
     |> add_data(data)
+  end
+
+  def default_player(player_id) do
+    %{
+      hero: Firestone.Engine.Entities.create("Rexxar", :hero, player_id),
+      board: [],
+      graveyard: [],
+      hand: [],
+      deck: [],
+      fatigue: 0,
+      mana: 0,
+      mana_crystals: 0
+    }
+  end
+
+  def add_player_data(state, player_id, data) do
+    initial_player = put_in(state.players[player_id], default_player(player_id))
+
+    for {key, val} <- data, reduce: initial_player do
+      state ->
+        cond do
+          key in [:hero] ->
+            add_player_entity(state, player_id, key, val)
+
+          key in [:board, :hand, :deck, :graveyard] ->
+            add_player_entity_list(state, player_id, key, val)
+
+          true ->
+            put_in(state.players[player_id][key], val)
+        end
+    end
+  end
+
+  def add_player_entity(state, player_id, type, data) do
+    entity = Firestone.Engine.Entities.create(data, type, state.counter)
+
+    state
+    |> put_in([:players, player_id, type], entity)
+    |> update_in([:counter], &(&1 + 1))
+  end
+
+  def add_player_entity_list(state, player_id, type, list) do
+    entities =
+      for {data, c} <- Enum.with_index(list, state.counter) do
+        Firestone.Engine.Entities.create(data, type, c)
+      end
+
+    state
+    |> update_in([:players, player_id, type], &(entities ++ &1))
+    |> update_in([:counter], &(&1 + length(entities)))
+  end
+
+  def add_data(state, data) do
+    for {key, val} <- data, reduce: state do
+      state ->
+        cond do
+          key in [:turn] -> add_turn_data(state, val)
+          true -> put_in(state[key], val)
+        end
+    end
+  end
+
+  def add_turn_data(state, data) do
+    for {key, val} <- data, reduce: state do
+      state ->
+        cond do
+          key in [:cards_played, :minions_summoned] -> add_turn_entity_list(state, key, val)
+          true -> put_in(state.turn[key], val)
+        end
+    end
+  end
+
+  def add_turn_entity_list(state, key, list) do
+    entities =
+      for {data, c} <- Enum.with_index(list, state.counter) do
+        Firestone.Engine.Entities.create(data, key, c)
+      end
+
+    state
+    |> update_in([:turn, key], &(&1 ++ entities))
+    |> update_in([:counter], &(&1 + length(entities)))
   end
 
   def player(state, player_id, key),
